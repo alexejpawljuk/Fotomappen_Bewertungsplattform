b6c0be98eb51a32753be97a97daf53ebc6048e46
Pawljuk Alexej <pawljuk-alexej@hotmail.com>
2025-08-07 16:21:32 +0200

    Add registration form to /signup page

--- imports/ui/SignupPage/SignupPage.tsx
+++ imports/ui/SignupPage/SignupPage.tsx
@@ -1,15 +1,52 @@
-import React from 'react';
-import {Typography} from "antd";
+import React, {useState} from 'react';
+import {Button, Input, message, Space, Typography} from "antd";
+import { useNavigate } from "react-router-dom";
+import validator from "validator";
+import {LockOutlined} from "@ant-design/icons";
+import {publicRoutes} from "/imports/utils/constans/routes";
 
 interface Props {
     // define your props here
 }
 
 export const SignupPage: React.FC<Props> = ({}) => {
+    const [email, setEmail] = useState("")
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate()
+
+    const handleSubmit = () => {
+        const cleanedEmail = email.trim()
+        const cleanedUsername = username.trim()
+
+        if(!validator.isEmail(cleanedEmail)) {
+            return message.error("Email is invalid")
+        }
+
+        if (cleanedUsername.length < 3) {
+            return message.error("Username is too short")
+        }
+
+        if (password.length < 8) {
+            return message.error("Password is too short")
+        }
+    }
+
     return (
-        <Typography.Title level={2}>
-            SignupPage component
-        </Typography.Title>
+        <Space direction="vertical">
+            <Typography.Title level={2}>Create new account</Typography.Title>
+
+            <Input placeholder={"email"} value={email} addonBefore={"@"} onChange={e => setEmail(e.target.value)}/>
+            <Input placeholder={"username"} value={username} onChange={e => setUsername(e.target.value)}/>
+            <Input.Password placeholder={"Password"} value={password} type={"password"} prefix={<LockOutlined />} onChange={e => setPassword(e.target.value)}/>
+
+            <Button type="primary" onClick={handleSubmit}>Sign Up</Button>
+
+            <Typography>
+                Already have an account?
+                <Button type="link" onClick={() => navigate(publicRoutes.login.path)}>Login</Button>
+            </Typography>
+        </Space>
     );
 };
 
