f9da1063f21769b124f9bbaf8ba86f92868766c7
Pawljuk Alexej <pawljuk-alexej@hotmail.com>
2025-08-07 22:17:16 +0200

    Add login form to /login page

--- imports/ui/LoginPage/LoginPage.tsx
+++ imports/ui/LoginPage/LoginPage.tsx
@@ -1,16 +1,44 @@
-import React from 'react';
-import {Typography} from "antd";
+import React, {useState} from 'react';
+import {Button, Input, message, Space, Typography} from "antd";
+import {LockOutlined} from "@ant-design/icons";
+import {publicRoutes} from "/imports/utils/constans/routes";
+import {useNavigate} from "react-router-dom";
+import validator from "validator";
 
 interface Props {
     // define your props here
 }
 
 export const LoginPage: React.FC<Props> = ({}) => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate()
+
+    const handleSubmit = () => {
+        const cleanedEmail = email.trim()
+
+        if(!validator.isEmail(cleanedEmail)) {
+            return message.error("Email is invalid")
+        }
+
+        if (password.length < 8) {
+            return message.error("Password is too short")
+        }
+    }
+
     return (
-        <Typography.Title level={2}>
-            LoginPage component
-        </Typography.Title>
+        <Space direction="vertical">
+            <Typography.Title level={2}>Log into your account</Typography.Title>
+
+            <Input placeholder={"email"} value={email} addonBefore={"@"} onChange={e => setEmail(e.target.value)}/>
+            <Input.Password placeholder={"Password"} value={password} type={"password"} prefix={<LockOutlined />} onChange={e => setPassword(e.target.value)}/>
+
+            <Button type="primary" onClick={handleSubmit}>Login</Button>
+
+            <Typography>
+                Registration
+                <Button type="link" onClick={() => navigate(publicRoutes.signup.path)}>Login</Button>
+            </Typography>
+        </Space>
     );
 };
-
-
