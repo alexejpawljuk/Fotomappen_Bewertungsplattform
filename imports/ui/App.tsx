3038ce0e4d4156d03cb07916789ce6e4f5d2755d
Alexej Pawljuk <pawljuk-alexej@hotmail.com>
2025-08-13 14:28:22 +0200

    Extract routing into a separate React component

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,49 +1,18 @@
 import React from 'react';
-import {useTracker} from "meteor/react-meteor-data";
-import {Meteor} from "meteor/meteor";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {ConfigProvider, message} from "antd";
-import {protectedRoutes, publicRoutes} from "/imports/utils/constans/routes";
-import {LoadingOutlined} from "@ant-design/icons";
 import {MESSAGE_DURATION} from "/imports/ui/config";
+import {Router} from "/imports/ui/Router/Router";
 
 
-/**
- * Defines the type for the user ID on login
- *
- * null - not logged in
- * undefined - loading data
- * string - logged in (user id)
- */
+
 export type AppUserIdModel = string | undefined | null
 
 export const App = () => {
-    // @ts-ignore
-    const { userId, loggingIn } = useTracker(() => ({
-        userId: Meteor.userId(),
-        loggingIn: Meteor.loggingIn(),
-    }), []);
     message.config({duration: MESSAGE_DURATION})
 
-    const routes = [
-        ...(userId === null ?
-            [...Object.values(publicRoutes)] :
-            [...Object.values(protectedRoutes), ...Object.values(publicRoutes)])
-    ]
-
-    if (userId === undefined) {
-        return <LoadingOutlined/>
-    }
-
     return (
-        <BrowserRouter>
-            <ConfigProvider>
-                <Routes>
-                    {routes.map((route) => (
-                        <Route key={route.path} path={route.path} element={route.element}/>
-                    ))}
-                </Routes>
-            </ConfigProvider>
-        </BrowserRouter>
+        <ConfigProvider>
+            <Router/>
+        </ConfigProvider>
     )
 }
---
+++ imports/ui/Router/Router.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Flex, Spin} from "antd";
+import {protectedRoutes, publicRoutes} from "/imports/utils/constans/routes";
+import {LoadingOutlined} from "@ant-design/icons";
+import {Meteor} from "meteor/meteor";
+import { useTracker } from "meteor/react-meteor-data";
+
+interface RouterProps {
+    // TODO: define props here
+}
+
+/**
+ * Defines the type for the user ID on login
+ *
+ * null - not logged in
+ * undefined - loading data
+ * string - logged in (user id)
+ */
+export const Router: React.FC<RouterProps> = ({}) => {
+
+    // @ts-ignore
+    const {userId, loggingIn} = useTracker(() => ({
+        userId: Meteor.userId(),
+        loggingIn: Meteor.loggingIn(),
+    }), []);
+
+    const routes = [
+        ...(userId === null ?
+            [...Object.values(publicRoutes)] :
+            [...Object.values(protectedRoutes), ...Object.values(publicRoutes)])
+    ]
+
+    if (userId === undefined) {
+        return (
+            <Flex justify={"center"} align={"center"} style={{height: "100%"}}>
+                <Spin indicator={<LoadingOutlined style={{ fontSize: 48 }} spin />} />
+            </Flex>
+        )
+    }
+
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                {routes.map((route) => (
+                    <Route key={route.path} path={route.path} element={route.element}/>
+                ))}
+            </Routes>
+        </BrowserRouter>
+    );
+};
