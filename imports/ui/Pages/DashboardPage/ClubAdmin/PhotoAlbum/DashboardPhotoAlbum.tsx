5efd45e9aac5f45edc62c5cdaf4465d7eac8d103
home <pawljuk-alexej@hotmail.com>
2025-10-17 17:55:51 +0200

    Change method of retrieving photo album title in album header

--- imports/ui/Pages/DashboardPage/ClubAdmin/PhotoAlbum/DashboardPhotoAlbum.tsx
+++ imports/ui/Pages/DashboardPage/ClubAdmin/PhotoAlbum/DashboardPhotoAlbum.tsx
@@ -3,14 +3,8 @@ import {useParams} from "react-router-dom";
 import {DashboardContentTitle} from "/imports/ui/Pages/DashboardPage/DashboardContentTitle";
 import {AddPhotoPanel} from "/imports/ui/Pages/DashboardPage/ClubAdmin/PhotoAlbum/components/AddPhotoPanel";
 import {DashboardClubAdminLayout} from "/imports/ui/Pages/DashboardPage/ClubAdmin/DashboardClubAdminLayout";
-import {Meteor} from "meteor/meteor";
-import {PhotoAlbumMethods} from "/imports/api/names";
-import {
-    MethodGetPhotoAlbumByIDRequestModel,
-    MethodGetPhotoAlbumByIDResponseModel
-} from "/imports/api/PhotoAlbum/models";
-import {message} from "antd";
 import {PhotoList} from "/imports/ui/Pages/DashboardPage/ClubAdmin/PhotoAlbum/components/PhotoList";
+import {PhotoAlbumService} from "/imports/ui/Services/PhotoAlbumService";
 
 
 interface DashboardPhotoAlbumProps {
@@ -20,21 +14,11 @@ interface DashboardPhotoAlbumProps {
 export const DashboardPhotoAlbum: React.FC<DashboardPhotoAlbumProps> = ({}) => {
     const {albumId} = useParams()
     const [title, setTitle] = useState("")
+    const {photoAlbumsList} = PhotoAlbumService()
 
     useEffect(() => {
-        if (!albumId) return
-
-        const params: MethodGetPhotoAlbumByIDRequestModel = {
-            albumId
-        }
-        Meteor.call(PhotoAlbumMethods.GET_PHOTO_ALBUM_BY_ID, params, (err: any, res: MethodGetPhotoAlbumByIDResponseModel) => {
-            if (err) {
-                console.error(err)
-                return message.error(`Album with id ${albumId} not found`)
-            }
-            if (!res.photoAlbum) return message.error(`Album with id ${albumId} not found`)
-            setTitle(res.photoAlbum.title)
-        })
+        const album = photoAlbumsList.find(album => album.albumId === albumId)
+        setTitle(album?.title ?? "Title undefined")
     }, [albumId]);
 
     return (
