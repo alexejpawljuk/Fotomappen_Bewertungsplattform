e7df06565ca6e5b5ddeffe7bb23c27aa0ac33ed0
home <pawljuk-alexej@hotmail.com>
2025-10-18 13:32:24 +0200

    Remove email verification notification

--- imports/ui/Pages/DashboardPage/DashboardLayout.tsx
+++ imports/ui/Pages/DashboardPage/DashboardLayout.tsx
@@ -1,8 +1,5 @@
-import React, {useEffect} from 'react';
-import {Flex, message, Typography} from "antd";
-import {useTracker} from "meteor/react-meteor-data";
-import {Meteor} from "meteor/meteor"
-import {Verification} from "/imports/utils/constans/text";
+import React from 'react';
+import {Flex, Typography} from "antd";
 import {MainLayout} from "/imports/ui/Layout/MainLayout";
 
 
@@ -10,27 +7,27 @@ interface DashboardProps {
     children?: React.ReactNode
 }
 
-const useNotificationOfVerification = () => {
-    const { userId, verified, loggingIn } = useTracker(() => {
-        const user = Meteor.user();
-        return {
-            userId: Meteor.userId(),
-            verified: !!user?.emails?.[0]?.verified,
-            loggingIn: Meteor.loggingIn(),
-        };
-    });
-
-    useEffect(() => {
-        if (loggingIn) return;
-        if (!userId) return;
-        if (verified) return;
-
-        message.warning(Verification.UNVERIFIED);
-    }, [userId, verified, loggingIn]);
-}
+// const useNotificationOfVerification = () => {
+//     const { userId, verified, loggingIn } = useTracker(() => {
+//         const user = Meteor.user();
+//         return {
+//             userId: Meteor.userId(),
+//             verified: !!user?.emails?.[0]?.verified,
+//             loggingIn: Meteor.loggingIn(),
+//         };
+//     });
+//
+//     useEffect(() => {
+//         if (loggingIn) return;
+//         if (!userId) return;
+//         if (verified) return;
+//
+//         message.warning(Verification.UNVERIFIED);
+//     }, [userId, verified, loggingIn]);
+// }
 
 export const DashboardLayout: React.FC<DashboardProps> = ({children}) => {
-    useNotificationOfVerification()
+    // useNotificationOfVerification()
 
     return (
         <MainLayout>
